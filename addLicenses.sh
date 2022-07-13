dc8152dc465472252a38d3d72ec3b4cad9050628
Li_Mengxun <limengxun45@outlook.com>
2022-07-13 16:31:26 +0800

    delete unnecessary

--- addLicenses.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
- cat ~/Code/headerTemplate.txt | tbx_codeHeader ./evaluation -c '{"date":"June 2022"}'
- cat ~/Code/headerTemplate.txt | tbx_codeHeader ./nnUNetTMJ -c '{"date":"June 2022"}'
- cat ~/Code/headerTemplate.txt | tbx_codeHeader ./UNetPPTMJ -c '{"date":"June 2022"}'
- cat ~/Code/headerTemplate.txt | tbx_codeHeader ./packages/toolBox -c '{"date":"June 2022"}'
- cat ~/Code/headerTemplate.txt | tbx_codeHeader ./packages/labelSys -c '{"date":"June 2022"}'
